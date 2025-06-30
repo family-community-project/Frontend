0f14249112aaa025f6056c6c0d52d5b171cc1eed
Eom-Ye-rim <dpfladlwkdrn@naver.com>
2025-06-30 15:20:30 +0900

    ci: Update workflow to deploy from develop branch

--- family/next.config.mjs
+++ family/next.config.mjs
@@ -7,17 +7,6 @@
 // export default nextConfig;
 
 
-// import type { NextConfig } from 'next'
-
-// const nextConfig: NextConfig = {
-//   output: 'export', // `next export` 시 out/ 폴더 생성
-//   reactStrictMode: true,
-//   trailingSlash: true, // S3 업로드 시 
-// }
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // `next export` 시 out/ 폴더 생성
