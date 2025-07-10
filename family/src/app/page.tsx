a9fae4803fc12873cbedc075370830045d367d82
hyeok <jhyouk1015@gamil.com>
2025-07-10 14:40:15 +0900

    feat : dropdownUI

--- family/src/app/page.tsx
+++ family/src/app/page.tsx
@@ -1,3 +1,23 @@
+import { DropDown } from "./components/dropdown";
+
 export default function Home() {
-  return;
+  return (
+    <div className="w-[120px] m-auto">
+      <br></br>
+      <br></br>
+      <br></br>
+      <DropDown name="" option={["옵션1", "옵션2"]} placeholder={"작성하기"} />
+      <br />
+      <DropDown name="" option={["옵션1", "옵션2"]} placeholder={"작성하기"} />
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <DropDown name="" option={["옵션1", "옵션2"]} placeholder={"작성하기"} />
+      <br />
+      <br />
+      <br />
+      <DropDown name="" option={["옵션1", "옵션2"]} placeholder={"작성하기"} />
+    </div>
+  );
 }
