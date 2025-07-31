d96c3482fd65a03af585ba7084be767ad352ce4d
mer <117734307+merx88@users.noreply.github.com>
2025-08-01 00:40:50 +0900

    Revert "Feat/chip"

--- family/src/app/page.tsx
+++ family/src/app/page.tsx
@@ -1,26 +1,8 @@
-import Chip from "./components/chip";
-import { DropDown } from "./components/dropdown";
-
+import { Button } from "./components/button";
 export default function Home() {
   return (
-    <div className="w-[120px] m-auto">
-      <br></br>
-      <br></br>
-      <br></br>
-      <DropDown name="" option={["옵션1", "옵션2"]} placeholder={"작성하기"} />
-      <br />
-      <DropDown name="" option={["옵션1", "옵션2"]} placeholder={"작성하기"} />
-      <br></br>
-      <br></br>
-      <br></br>
-      <Chip label="예시 라벨" />
-      <br></br>
-      <DropDown name="" option={["옵션1", "옵션2"]} placeholder={"작성하기"} />
-      <br></br>
-      <br />
-      <br />
-      <br />
-      <DropDown name="" option={["옵션1", "옵션2"]} placeholder={"작성하기"} />
+    <div>
+      <Button text="작성하기" size="M" type="outline" />
     </div>
   );
 }
